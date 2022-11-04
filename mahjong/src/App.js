592a3415795ca1457351b55c9be93e752996b38c
niZaliznyak <novieasdf@gmail.com>
2022-11-04 19:57:43 +0200

    remove handler from active/opened card

--- mahjong/src/App.js
+++ mahjong/src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState, useMemo } from 'react';
-import Card from "./components/Card/Card"
+import Card from './components/Card/Card';
 import style from './App.module.css';
-import { generateShuffledPrimeNumbers } from "./utils";
+import { generateShuffledPrimeNumbers } from './utils';
 
 function App() {
   const shuffledDeck = useMemo(() => generateShuffledPrimeNumbers(), []);
@@ -9,7 +9,7 @@ function App() {
   const [currentIndex, setCurrentIndex] = useState([]);
 
   useEffect(() => {
-    let timer = setTimeout(() => setShow([]), 5000);
+    const timer = setTimeout(() => setShow([]), 5000);
 
     return () => {
       clearTimeout(timer);
@@ -24,31 +24,41 @@ function App() {
     if (currentIndex.length !== 0 && currentIndex[0][0] === number) {
       setShow([...show, number]);
       setCurrentIndex([]);
+    } else if (currentIndex.length === 1 && currentIndex[0][0] !== number) {
+      setCurrentIndex(() => [...currentIndex, [number, index]]);
+      setTimeout(() => {
+        setCurrentIndex([]);
+      }, 1000);
     } else {
-        if (currentIndex.length === 1 && currentIndex[0][0] !== number) {
-          setCurrentIndex(() => [...currentIndex, [number, index]]);
-          setTimeout(() => {
-            setCurrentIndex([]);
-          }, 1000);
-        } else {
-          setCurrentIndex(() => [...currentIndex, [number, index]]);
-        }
+      setCurrentIndex(() => [...currentIndex, [number, index]]);
     }
-  }
+  };
+
+  const checkActive = (number, index) =>
+    currentIndex.find(number => number[1] === index);
+  const shouldShow = (number, index) =>
+    show.includes(number) || currentIndex.find(num => num[1] === index);
+
+  const renderCard = (number, index) => {
+    const acitve = checkActive(number, index);
+    const show = shouldShow(number, index);
+    return (
+      <React.Fragment key={index}>
+        <Card
+          number={number}
+          acitve={acitve}
+          show={show}
+          onClick={show ? undefined : onTurnCard(number, index)}
+        />
+      </React.Fragment>
+    );
+  };
 
   return (
     <div className={style.wrapper}>
       <h1>Mahjong</h1>
       <div className={style.desk}>
-        {shuffledDeck.map((number, index) => (
-          <React.Fragment key={index + index}>
-            <Card
-              number={number}
-              acitve={currentIndex.find(num => num[1] === index)}
-              show={show.includes(number) || currentIndex.find(num => num[1] === index)}
-              onClick={onTurnCard(number, index)}/>
-          </React.Fragment>
-        ))}
+        {shuffledDeck.map((number, index) => renderCard(number, index))}
       </div>
     </div>
   );
