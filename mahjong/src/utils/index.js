bef36280fb830c1239549469beb08646fbf22d20
Nikita Zalizniak <novieasdf@gmail.com>
2022-11-13 20:13:31 +0200

    delete unnecessary shuffle

--- mahjong/src/utils/index.js
+++ mahjong/src/utils/index.js
@@ -5,25 +5,25 @@ const isPrime = num => {
   if (num % 2 === 0 && num > 2) {
     return false;
   }
-    const s = Math.sqrt(num);
-    for (let i = 3; i <= s; i += 2) {
-      if (num % i === 0) return false;
-    }
+  const s = Math.sqrt(num);
+  for (let i = 3; i <= s; i += 2) {
+    if (num % i === 0) return false;
+  }
 
-    return true;
+  return true;
 };
-  
-  const shuffle = array => {
-    const newArray = [...array];
-    newArray.sort(() => Math.random() - 0.5);
-    return newArray;
+
+const shuffle = array => {
+  const newArray = [...array];
+  newArray.sort(() => Math.random() - 0.5);
+  return newArray;
 };
-  
-  export const generateShuffledPrimeNumbers = () => {
-    const primeNumbers = Array.from(Array(60), (_, i) => i + 1).filter(num =>
-      isPrime(num),
-    );
-    const shuffledSliced = shuffle(primeNumbers).slice(0, -1);
-    return shuffle(shuffledSliced.concat(shuffledSliced));
+
+export const generateShuffledPrimeNumbers = () => {
+  const primeNumbers = Array.from(Array(55), (_, i) => i + 1).filter(num =>
+    isPrime(num)
+  );
+
+  const shuffledSliced = shuffle([...primeNumbers, ...primeNumbers]);
+  return shuffledSliced;
 };
-  
