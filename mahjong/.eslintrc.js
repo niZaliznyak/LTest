7370e636858c3fb535798e594abd9ed37b243e79
Nikita Zalizniak <novieasdf@gmail.com>
2022-11-14 18:08:03 +0200

    add eslint

---
+++ mahjong/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard-with-typescript'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    curly: 'error',
+    quotes: ['error', 'single'],
+    'react/prop-types': 0,
+    semi: ['error', 'always']
+  }
+};
